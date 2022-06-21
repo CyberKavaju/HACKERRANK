14f39a5a0acc94746c166b640a10930813ee8859
William Wright <william@rhodian.net>
2022-06-21 09:42:21 -0400

    challange day2 and 3

--- 30daychallange/day2-operators.php
+++ 30daychallange/day2-operators.php
@@ -1,1 +1,13 @@
 <?php
+$meal_cost = 12.00;
+$tip_percent = 20;
+$tax_percent = 8;
+
+function solve($meal_cost, $tip_percent, $tax_percent) {
+    $tip = $meal_cost * ($tip_percent / 100);
+    $tax = $meal_cost * ($tax_percent / 100);
+    $total = $meal_cost + $tip + $tax;
+    fwrite(STDOUT, round($total));//in hacker rank is been run with fopen() and fwrite() instead of echo because is run in the comand line and not in the browser
+}
+
+solve($meal_cost, $tip_percent, $tax_percent);
